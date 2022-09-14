cf69b5acb724af313e33f309fdcfc1772e8c35d8
Hambalieu <hambalieujallow@gmail.com>
2022-09-14 10:35:33 -0700

    completed a problem of converting numbers into word using dictionaries

--- week3/dictionaries.py
+++ week3/dictionaries.py
@@ -12,10 +12,11 @@ print(customer["name"])
 
 ## Write a program that transate digits to words:
 
-# my_dict = {0:"Zero", 1: "One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine"}
+phone = input("Phone: ")
 
+my_dict = {"0":"Zero", "1": "One", "2":"Two", "3":"Three", "4":"Four", "5":"Five", "6":"Six", "7":"Seven", "8":"Eight", "9":"Nine"}
 
-# enter_phone = int(input(f'Phone: '))
-# for k,v in my_dict:
-#   if enter_phone in my_dict:
-#     print(v)
+output = ""
+for ch in phone:
+  output += my_dict.get(ch, "!") + " "
+print(output)
